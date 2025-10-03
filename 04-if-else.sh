a492196ba3445cdd0e708a677e3b60b78c1a80c4
Jaya-Nagendra <nagendra.44@outlook.com>
2025-10-03 15:56:01 +0530

    shell-script

--- 04-if-else.sh
+++ 04-if-else.sh
@@ -7,12 +7,9 @@ then
     echo "Given Number $NUMBER Is Greater then 100"
 else 
     echo "Given Number $NUMBER Is Lessthen 100"
-fi
+fi 
 
+# fi - Denotes end of the if-else block 
 
+# if [ "$NUMBER" -gt 100 ]   - The spacing is mandatory.
 
-# if [ "$NUMBER" -gt 100 ]; then
-#     echo "Given Number $NUMBER Is Greater than 100"
-# else 
-#     echo "Given Number $NUMBER Is Less than 100"
-# fi
---
+++ 05-install_my-sql.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then 
+echo "Error: This user $ID was not a root user"
+else 
+echo "$ID was a root user"
+yum install mysql -g
+fi
