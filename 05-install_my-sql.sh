b53cc299a08694e24d75b3e35866f2b4c16e461a
Jaya-Nagendra <nagendra.44@outlook.com>
2025-10-03 19:51:38 +0530

    shell-script

--- 05-install_my-sql.sh
+++ 05-install_my-sql.sh
@@ -4,9 +4,16 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then 
-echo "Error: This user $ID was not a root user"
+echo "Error: This user $ID was not a root user"  #If its not sucess also it will go for instalation but there linux will give error for avoiding this will use EXIT Code.
+exit 1 # you can give other than 0
 else 
 echo "$ID was a root user"
 fi
 
-yum install mysql -y
+yum install mysql -y  
+
+if [ $? -ne 0 ]
+then 
+echo "Error : Installing mysql is failed"
+else
+echo "The mysql installation was sucesss"
