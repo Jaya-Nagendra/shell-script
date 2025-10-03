e59ce1bbf522b64bd06b6a16cfd84459f411fc1d
Jaya-Nagendra <nagendra.44@outlook.com>
2025-10-03 21:09:09 +0530

    shell-script

--- 07-logs.sh
+++ 07-logs.sh
@@ -17,18 +17,18 @@ N="\e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo "Error : Installing $2 $R FAILED $N" 
+        echo -e "Error : Installing $2 $R FAILED $N" 
     else
-        echo "Installation $2 $G SUCESS $N"
+        echo -e "Installation $2 $G SUCESS $N"
     fi
 }
 
 if [ $ID -ne 0 ]
 then 
-    echo "$R Error: Your not a root user $N"  #If its not sucess also it will go for instalation but there linux will give error for avoiding this will use EXIT Code.
+    echo -e "$R Error: Your not a root user $N"  #If its not sucess also it will go for instalation but there linux will give error for avoiding this will use EXIT Code.
     exit 1 # you can give other than 0
 else 
-    echo "$G You are root user $N"
+    echo -e "$G You are root user $N"
 fi
 
 yum install mysql -y &>> $LOGFILE
