f9c3327e8bb82d7ec0e3aaeb94b3b5518f827960
Jaya-Nagendra <nagendra.44@outlook.com>
2025-10-03 15:18:57 +0530

    shell-script

--- 02-variables.sh
+++ 02-variables.sh
@@ -5,20 +5,20 @@ VARIABLE2=Nagendra
 
 echo "My Name is $VARIABLE1 $VARIABLE2"
 
-#For storng an inbuilt function out put in to an variable.
+#For storng an command out put in to an variable.
 
 DATE=$(date)
 
 echo "Dtae and Time is : $DATE"
 
-#For Dynamic inputs - Inputs from the customer while running time 
+#For Dynamic arguments - from the customer while running time
 
 VARIABLE3=$1
 VARIABLE4=$2
 
 echo "I am Learning : $1 - $2" 
 
-#While entering inputs in running time in console these are vissible if we want to hide them while entering in console.
+#While entering arguments in running time in console these are vissible if we want to hide them while entering in console.
 
 echo "Please Enter the user name"
 read USERNAME             #This entering value will be visible in console.
--- 03-data_types.sh
+++ 03-data_types.sh
@@ -7,4 +7,13 @@ NUMBER2=AAA
 
 SUM=$(($NUMBER1+$NUMBER2))
 
-echo "Total = $SUM"
+echo "Total = $SUM"         # The out put was 20 it will not consider NUMBER2.
+
+# Arrays 
+
+FRUITS=("Apple" "Banana" "Kiwi")
+
+echo "1st Fruit : ${FRUITS[0]}"
+
+echo "All Fruits : ${FRUITS[@]}"    # Print All fruits
+
