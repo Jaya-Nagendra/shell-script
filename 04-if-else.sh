68557be5b61cf75a6cdb599b4496520e592cd725
Jaya-Nagendra <nagendra.44@outlook.com>
2025-10-03 15:41:42 +0530

    shell-script

--- 04-if-else.sh
+++ 04-if-else.sh
@@ -2,9 +2,17 @@
 
 NUMBER=$1
 
-if[ $NUMBER -gt 100 ]
+if[ "$NUMBER" -gt 100 ];
 then 
     echo "Given Number $NUMBER Is Greater then 100"
 else 
     echo "Given Number $NUMBER Is Lessthen 100"
 fi
+
+
+
+# if [ "$NUMBER" -gt 100 ]; then
+#     echo "Given Number $NUMBER Is Greater than 100"
+# else 
+#     echo "Given Number $NUMBER Is Less than 100"
+# fi
