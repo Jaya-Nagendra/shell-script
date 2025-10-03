af573234422196c148fdf8f447003dd1c4e2cd2f
Jaya-Nagendra <nagendra.44@outlook.com>
2025-10-03 14:50:46 +0530

    shell-script

--- 02-variables.sh
+++ 02-variables.sh
@@ -17,3 +17,13 @@ VARIABLE3=$1
 VARIABLE4=$2
 
 echo "I am Learning : $1 - $2" 
+
+#While entering inputs in running time in console these are vissible if we want to hide them while entering in console.
+
+echo "Please Enter the user name"
+read USERNAME             #This entering value will be visible in console.
+
+echo "Please Enter your password"
+read -s PASSWORD           # This value was not vissible.
+
+echo "User name is : $USERNAME & Password is : $PASSWORD"
