36e085285207139c6a9e14d966233d0ccae433fd
Jaya-Nagendra <nagendra.44@outlook.com>
2025-10-03 14:40:24 +0530

    shell-script

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,3 +4,9 @@ VARIABLE1=Jaya
 VARIABLE2=Nagendra
 
 echo "My Name is $VARIABLE1 $VARIABLE2"
+
+#For storng an inbuilt function out put in to an variable.
+
+DATE=$(date)
+
+echo "Dtae and Time is : $DATE"
