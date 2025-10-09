4418d4852c0bdf20900c0556f08dc787918cc822
Jaya-Nagendra <nagendra.44@outlook.com>
2025-10-09 09:50:12 +0530

    Shell Script

--- 09-install_packages.sh
+++ 09-install_packages.sh
@@ -4,6 +4,9 @@
 
 ID=$(id -u)
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 R="\e[31m"
 
@@ -12,6 +15,8 @@ G="\e[32m"
 Y="\e[33m"
 
 N="\e[0m"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -34,11 +39,13 @@ echo "The all arguments passed are : $@"
 
 for package in $@
 do
-    yum list installed $package  #check installed or not 
+    yum list installed $package &>> $LOGFILE #check installed or not 
 
     if [ $? -ne 0 ]
     then 
-        yum install $package -y
+        yum install $package -y &>> $LOGFILE
         VALIDATE $1 "Installing $package"
     else
         echo -e "$package is allready installed ... $Y Skipping $N" 
+    fi
+done
