20aea37145ae19a4be5fe6fe70f2f729ac6782e2
Jaya-Nagendra <nagendra.44@outlook.com>
2025-10-09 09:46:22 +0530

    Shell Script

--- 09-install_packages.sh
+++ 09-install_packages.sh
@@ -13,12 +13,32 @@ Y="\e[33m"
 
 N="\e[0m"
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 ... $R FAILED $N"
+    else 
+        echo -e "$2 ... $G SUCCESS $N"
+    fi    
+
+}
+
 if [ $ID -ne 0 ]
 then 
-    echo -e "$R Error: Your not a root user $N"  #If its not sucess also it will go for instalation but there linux will give error for avoiding this will use EXIT Code.
-    exit 1 # you can give other than 0
+    echo -e "$R Error: Your not a root user $N"  
 else 
     echo -e "$G You are root user $N"
 fi
 
 echo "The all arguments passed are : $@"
+
+for package in $@
+do
+    yum list installed $package  #check installed or not 
+
+    if [ $? -ne 0 ]
+    then 
+        yum install $package -y
+        VALIDATE $1 "Installing $package"
+    else
+        echo -e "$package is allready installed ... $Y Skipping $N" 
