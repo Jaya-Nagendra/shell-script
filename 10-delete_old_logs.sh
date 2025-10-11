0b7b30dc95b03105f40fa2bfade487e110dc1a38
Jaya-Nagendra <nagendra.44@outlook.com>
2025-10-11 13:18:49 +0530

    shell Script

--- 10-delete_old_logs.sh
+++ 10-delete_old_logs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-SOURCE_DIR=/tmp/shellscript-logs
+SOURCE_DIR="/tmp/shellscript-logs"
 
 if [ ! -d $SORCE_DIR ] # -d will check weather the directory available or not 
 then
     echo -e "$R Source directory : $SOURCE_DIR does not exists. $N"
 fi
 
-FILES_TO_DELET=$(find . -type f -mtime +14 -name "*.log")
+FILES_TO_DELET=$(find $SORCE_DIR -type f -mtime +14 -name "*.log")
 
 while IFS= read -r line
 do
