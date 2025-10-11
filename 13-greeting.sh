ad664da698f1e33e4f8973a042379b8d1780c8e8
Jaya-Nagendra <nagendra.44@outlook.com>
2025-10-11 20:32:07 +0530

    shell Script

--- 13-greeting.sh
+++ 13-greeting.sh
@@ -15,6 +15,7 @@ while getopts ":n:w:h" opt; do
     case $opt in
         n) NAME="$OPTARG";;
         w) WISHES="$OPTARG";;
+        \?) echo "Invalid options: - "$OPTARG"" >&2; USAGE; exit;;
         h|*) USAGE; exit;;
      esac
 done
