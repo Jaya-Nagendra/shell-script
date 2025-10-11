02e3c11afe1c0087fcbd555603a9700ec3bdc609
Jaya-Nagendra <nagendra.44@outlook.com>
2025-10-11 19:48:44 +0530

    shell Script

---
+++ 13-greeting.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NAME=""
+WISHES=""
+
+USAGE(){
+    echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
+    echo "Options::"
+    echo "-n, Specify the name (mandatory)"
+    echo "-w, Specify the wishes (ex-good morning)"
+    echo "-h, Display help and exit"
+}
+
+while getopts ":n:w:h" opt; do
+    case $opt in
+        n) NAME="$OPTARG";;
+        w) WISHES="$OPTARG";;
+        h|*) USAGE; exit;;
+     esac
+done
