4b3b8b1b86f74c41b9cddab9434f6d8c8e0399e0
Jaya-Nagendra <nagendra.44@outlook.com>
2026-01-30 18:03:49 +0530

    script

--- 07-logs.sh
+++ 07-logs.sh
@@ -2,12 +2,13 @@
 USERID=$(id -u)
 
 LOGFOLDER="/var/log/shell"
-LOGFILE="$LOGFOLDER/$0.log"
+LOGFILE="/$LOGFOLDER/$0.log"
 
-if [USERID -ne 0 ]; then
+if [ $USERID -ne 0 ]; then
 echo "Run this script with root user"
 exit 1
 fi
+mkdir -p LOGFOLDER
 
 VALIDATE(){
     if [ $1 -eq 0 ]; then
