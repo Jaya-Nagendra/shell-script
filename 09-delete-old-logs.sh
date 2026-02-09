78a12e359047d6bb38b66e8d23e04e8dc9a2b6b7
Jaya-Nagendra <nagendra.44@outlook.com>
2026-02-09 10:04:05 +0530

    shell-script

--- 09-delete-old-logs.sh
+++ 09-delete-old-logs.sh
@@ -9,7 +9,7 @@ B="\e[34m"
 LOGS_DIR=/home/ec2-user/app-logs
 LOGS_FILE="$LOGS_DIR/$(basename "$0").log"
 
-if [ ! d $LOGS_DIR ]; then
+if [ ! -d $LOGS_DIR ]; then
     echo "$LOGS_DIR was not exist"
     exit 1
 fi
@@ -18,10 +18,10 @@ FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
 
 if [ -z "$FILES_TO_DELETE" ]; then
     echo "No files older than 14 days to delete."
-else
-while IFS= read -f filepath; do
+  else
+    while IFS= read -f filepath; do
     echo "deleting file "$filepath"
     rm -f $filepath
     echo "deleted file: $filepath"
-done <<< $FILES_TO_DELETE        
+    done <<< $FILES_TO_DELETE        
 fi
